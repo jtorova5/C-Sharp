2bcf98827e1e7dac01e9290de9d94f04fec7f86e
jtorova5 <jtorova5@gmail.com>
2024-07-02 07:45:41 -0500

    feat: se carga contenido teórico de la sesión 01_primeros_pasos

---
+++ 01_primeros_pasos/Program.cs
@@ -0,0 +1,12 @@
+﻿// Declaración de variables
+
+string nombre = "Jhonatan", apellido = "Toro Vásquez", documento = "1036402698";
+byte edad = 26;
+var clan = "Gates";
+
+// Impresión de variables
+Console.WriteLine(@$"Nombre: {nombre}; 
+Apellidos: {apellido}; 
+Edad: {edad}; 
+Documento: {documento}; 
+Clan: {clan}");
