3661cd05025fd486bc3e527f8e8dee2ec7a69bf5
jtorova5 <jtorova5@gmail.com>
2024-07-04 07:10:25 -0500

    feat: se carga contenido teórico de la sesión 03_ciclos

---
+++ 03_ciclos/Program.cs
@@ -0,0 +1,45 @@
+﻿// ciclo for
+
+using System.Net;
+
+for (int i = 0; i < 10; i++)
+{
+Console.WriteLine($"{i+1}. Hola mundo!");
+}
+
+// ciclo foreach
+
+string[] nombres = { "Juan", "Pedro", "Ana" };
+
+foreach (string nombre in nombres)
+{
+Console.WriteLine($"Hola {nombre}!");
+}
+
+// ciclo while
+
+bool bandera = true;
+
+while (bandera)
+{
+    Console.WriteLine("Sigue adelante");
+    Console.Write("¿Quieres repetir la iteración? ");
+    string? respuesta = Console.ReadLine();
+    if (respuesta?.ToLower() == "no")
+    {
+        bandera = false;
+    }
+}
+
+// ciclo do-while
+
+do
+{
+    Console.WriteLine("Sigue adelante");
+    Console.Write("¿Quieres repetir la iteración? ");
+    string? respuesta = Console.ReadLine();
+    if (respuesta?.ToLower() == "no")
+    {
+        break;
+    }
+} while (true);
