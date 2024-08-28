7936fa646c1038a2af139692e6fe98793d2d2fcb
jtorova5 <jtorova5@gmail.com>
2024-08-28 08:46:34 -0500

    feat: the connection with the DB in MySQL was created

--- Crud_Library/library/Program.cs
+++ Crud_Library/library/Program.cs
@@ -1,7 +1,23 @@
+using DotNetEnv;
+using library.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Load environment variables from .env file
+Env.Load();
+
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbPort = Environment.GetEnvironmentVariable("DB_PORT");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+var dbUser = Environment.GetEnvironmentVariable("DB_USER");
+var dbPassword = Environment.GetEnvironmentVariable("DB_password");
+var mySqlConnection = $"server={dbHost};port={dbPort};database={dbName};uid={dbUser};password={dbPassword}";
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(mySqlConnection, ServerVersion.Parse("8.0.20-mysql")));
 
 var app = builder.Build();
 
